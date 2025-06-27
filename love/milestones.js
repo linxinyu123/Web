b09f54f415f16f182d9a18e51ae067cf3ff5941e
Lestoy <51046689+linxinyu123@users.noreply.github.com>
2025-06-27 19:23:50 +0100

    Add files via upload

---
+++ love/milestones.js
@@ -0,0 +1,38 @@
+// milestones.js
+const milestones = [
+  {
+    date: '2025-04-06',
+    title: '她的生日，我们身边有对方',
+    message: '这顿晚餐去吃了日料，有你，有笑，有喜欢的食物。还给她拍了张照片，能陪你过生日，就是我最大的幸运。',
+    imgs: [
+      'https://lestoy.com/love/IMG_20250409_184958_250.jpg',
+      'https://lestoy.com/love/IMG_20250622_190421.jpg',
+      'https://lestoy.com/love/mmexport1744221095914.jpg',
+      'https://lestoy.com/love/IMG_20250409_180039.jpg',
+      'https://lestoy.com/love/IMG_20250409_180107.jpg',
+      'https://lestoy.com/love/IMG_20250409_180117.jpg'
+    ]
+  },
+  {
+    date: '2025-03-04',
+    title: '我们一起去Cascais啦！',
+    message: '终于去到了久违的地方，还记得日落时牵着她的手在码头散步。',
+    imgs: [
+      'https://lestoy.com/love/IMG_20250409_181930.jpg',
+      'https://lestoy.com/love/IMG_20250304_193658.jpg',
+      'https://lestoy.com/love/IMG_20250304_180401_1.jpg',
+      'https://lestoy.com/love/IMG_20250304_193420.jpg',
+      'https://lestoy.com/love/IMG_20250304_152135.jpg',
+      'https://lestoy.com/love/IMG_20250304_144501.jpg'
+    ]
+  },
+  {
+    date: '2024-08-10',
+    title: '这次我们赴约，去了罗卡角',
+    message: '去吃了东方滋味，感觉还不错。那天阳光很好，我们终于一起站在了欧洲大陆的最西端。',
+    imgs: [
+      'https://lestoy.com/love/IMG_20240811_170916.jpg',
+      'https://lestoy.com/love/IMG_20240811_214451.jpg'
+    ]
+  }
+];
