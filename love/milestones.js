8eaf14157df5b07bd117781576dba648e61eade5
Lestoy <51046689+linxinyu123@users.noreply.github.com>
2025-06-27 21:03:34 +0100

    Update milestones.js

--- love/milestones.js
+++ love/milestones.js
@@ -1,24 +1,15 @@
 const milestones = [
   {
-    "date": "2025-08-10",
-    "title": "666",
-    "message": "6666",
-    "imgs": [
-      "imgs/1751053742361_Screenshot_2025-02-06-22-11-21-548_lockscreen - 副本.jpg",
-      "imgs/1751053742361_微信图片_2025-01-23_132823_968.png"
-    ]
-  },
-  {
     "date": "2025-04-06",
     "title": "她的生日，我们身边有对方",
     "message": "这顿晚餐去吃了日料，有你，有笑，有喜欢的食物。还给她拍了张照片，能陪你过生日，就是我最大的幸运。",
     "imgs": [
-      "https://lestoy.com/love/IMG_20250409_184958_250.jpg",
-      "https://lestoy.com/love/IMG_20250622_190421.jpg",
-      "https://lestoy.com/love/mmexport1744221095914.jpg",
-      "https://lestoy.com/love/IMG_20250409_180039.jpg",
-      "https://lestoy.com/love/IMG_20250409_180107.jpg",
-      "https://lestoy.com/love/IMG_20250409_180117.jpg"
+      "https://lestoy.com/love/imgs/IMG_20250409_184958_250.jpg",
+      "https://lestoy.com/love/imgs/IMG_20250622_190421.jpg",
+      "https://lestoy.com/love/imgs/mmexport1744221095914.jpg",
+      "https://lestoy.com/love/imgs/IMG_20250409_180039.jpg",
+      "https://lestoy.com/love/imgs/IMG_20250409_180107.jpg",
+      "https://lestoy.com/love/imgs/IMG_20250409_180117.jpg"
     ]
   },
   {
@@ -26,12 +17,12 @@ const milestones = [
     "title": "我们一起去Cascais啦！",
     "message": "终于去到了久违的地方，还记得日落时牵着她的手在码头散步。",
     "imgs": [
-      "https://lestoy.com/love/IMG_20250409_181930.jpg",
-      "https://lestoy.com/love/IMG_20250304_193658.jpg",
-      "https://lestoy.com/love/IMG_20250304_180401_1.jpg",
-      "https://lestoy.com/love/IMG_20250304_193420.jpg",
-      "https://lestoy.com/love/IMG_20250304_152135.jpg",
-      "https://lestoy.com/love/IMG_20250304_144501.jpg"
+      "https://lestoy.com/love/imgs/IMG_20250409_181930.jpg",
+      "https://lestoy.com/love/imgs/IMG_20250304_193658.jpg",
+      "https://lestoy.com/love/imgs/IMG_20250304_180401_1.jpg",
+      "https://lestoy.com/love/imgs/IMG_20250304_193420.jpg",
+      "https://lestoy.com/love/imgs/IMG_20250304_152135.jpg",
+      "https://lestoy.com/love/imgs/IMG_20250304_144501.jpg"
     ]
   },
   {
@@ -39,8 +30,8 @@ const milestones = [
     "title": "这次我们赴约，去了罗卡角",
     "message": "去吃了东方滋味，感觉还不错。那天阳光很好，我们终于一起站在了欧洲大陆的最西端。",
     "imgs": [
-      "https://lestoy.com/love/IMG_20240811_170916.jpg",
-      "https://lestoy.com/love/IMG_20240811_214451.jpg"
+      "https://lestoy.com/love/imgs/IMG_20240811_170916.jpg",
+      "https://lestoy.com/love/imgs/IMG_20240811_214451.jpg"
     ]
   }
 ];
