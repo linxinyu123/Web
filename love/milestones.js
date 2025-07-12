758f2b0df173c601359838900870e102c5c91994
Lestoy <51046689+linxinyu123@users.noreply.github.com>
2025-07-12 15:22:14 +0100

    更新 milestones.js

--- love/milestones.js
+++ love/milestones.js
@@ -7,5 +7,14 @@ const milestones = [
       "https://lestoy.com/love/imgs/IMG_20240811_170916.jpg",
       "https://lestoy.com/love/imgs/IMG_20240811_214451.jpg"
     ]
+  },
+  {
+    "date": "2024-08-10",
+    "title": "这次我们赴约，去了罗卡角",
+    "message": "去吃了东方滋味，感觉还不错。那天阳光很好，我们终于一起站在了欧洲大陆的最西端。",
+    "imgs": [
+      "imgs/1000035740.jpg",
+      "imgs/1000035739.jpg"
+    ]
   }
 ];
