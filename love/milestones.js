f13de617b4cc2a75bad195110b747034d88cbaff
Lestoy <51046689+linxinyu123@users.noreply.github.com>
2025-07-12 16:39:13 +0100

    更新 milestones.js

--- love/milestones.js
+++ love/milestones.js
@@ -1,5 +1,24 @@
 const milestones = [
   {
+    "date": "2024-11-03",
+    "title": "我们去动物园啦!",
+    "message": "那天咱俩逛了一整个动物园,见到了许多小动物和大动物朋友们,特别是那位小土拨鼠,但有点遗憾的是没有看到心心念念的卡皮巴拉😭",
+    "imgs": [
+      "https://lestoy.com/love/imgs/1000035788.jpg",
+      "https://lestoy.com/love/imgs/1000035786.jpg",
+      "https://lestoy.com/love/imgs/1000035784.jpg",
+      "https://lestoy.com/love/imgs/1000035787.jpg",
+      "https://lestoy.com/love/imgs/1000035783.jpg",
+      "https://lestoy.com/love/imgs/1000035781.jpg",
+      "https://lestoy.com/love/imgs/1000035779.jpg",
+      "https://lestoy.com/love/imgs/1000035785.jpg",
+      "https://lestoy.com/love/imgs/1000035782.jpg",
+      "https://lestoy.com/love/imgs/1000035778.jpg",
+      "https://lestoy.com/love/imgs/1000035780.jpg",
+      "https://lestoy.com/love/imgs/1000035777.jpg"
+    ]
+  },
+  {
     "date": "2024-08-10",
     "title": "这次我们赴约，去了罗卡角",
     "message": "去吃了东方滋味，感觉还不错。那天阳光很好，我们终于一起站在了欧洲大陆的最西端。",
